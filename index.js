f5155bc9752481ee56cd191cd37159c231225cc2
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2019-08-11 12:46:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const walk = require('acorn-walk')
 const check = (code, ecmaVersion) =>
   walk.simple(acorn.parse(code, {ecmaVersion}), {})
 
-module.exports = (code = code, ecmaVersion = 5) => {
+module.exports = (code, ecmaVersion = 5) => {
   try {
     check(code, ecmaVersion)
     return true
