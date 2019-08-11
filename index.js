bc3a90c7d01e0e6ee4b879bd14f009885feefb0e
LeonFedotov <leonf@wix.com>
2019-08-11 12:48:10 +0300

    cleanup

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const acorn = require('acorn')
-const walk = require('acorn-walk')
 
-const check = (code, ecmaVersion) =>
-  walk.simple(acorn.parse(code, {ecmaVersion}), {})
+const check = (code, ecmaVersion) => acorn.parse(code, {ecmaVersion})
 
 module.exports = (code = code, ecmaVersion = 5) => {
   try {
